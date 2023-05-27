931a9b7724c42315753c8cbccc13d6f34e4941dd
Liran Mery <liran.mery@gong.io>
2023-05-27 15:48:03 +0300

    todo list

--- src/App.tsx
+++ src/App.tsx
@@ -1,5 +1,33 @@
+import { useState } from "react";
+
 function App() {
-  return <div></div>;
+  const [list, setList] = useState<string[]>([]);
+
+  const handleSubmit = (e: React.FormEvent<HTMLFormElement>) => {
+    e.preventDefault();
+
+    const target = e.target as typeof e.target & {
+      name: { value: string };
+    };
+
+    const name = target.name.value;
+
+    setList([...list, name]);
+  };
+
+  return (
+    <div>
+      <ul>
+        {list.map((item) => (
+          <li key={item}>{item}</li>
+        ))}
+      </ul>
+      <form onSubmit={handleSubmit}>
+        <input type="text" name="name" />
+        <button>Add</button>
+      </form>
+    </div>
+  );
 }
 
 export default App;
