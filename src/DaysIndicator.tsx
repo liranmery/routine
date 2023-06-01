25022d5fead420a3c9ac08fbda321ca03d4b5526
Liran Mery <liran.mery@gong.io>
2023-06-01 09:54:39 +0300

    sync date effect

--- src/DaysIndicator.tsx
+++ src/DaysIndicator.tsx
@@ -1,6 +1,7 @@
 import { differenceInCalendarDays } from "date-fns";
 import styles from "./DaysIndicator.module.css";
 import { DOTS_SIZE } from "./consts";
+import { useEffect, useState } from "react";
 
 interface DaysIndicatorProps {
   date: string;
@@ -8,17 +9,31 @@ interface DaysIndicatorProps {
 }
 
 export function DaysIndicator({ date, max }: DaysIndicatorProps) {
-  const daysDiff = differenceInCalendarDays(new Date(), new Date(date));
+  const [currentDate, setCurrentDate] = useState(new Date());
+
+  const syncDate = () => {
+    const interval = setInterval(() => {
+      setCurrentDate(new Date());
+    }, 1000);
+
+    return () => clearInterval(interval);
+  };
+
+  useEffect(syncDate, []);
+
+  const daysDiff = differenceInCalendarDays(currentDate, new Date(date));
 
   return (
     <div className={styles.root}>
       {[...Array(DOTS_SIZE)].map((_, index) => {
+        const dotRatio = (index + 1) * (1 / DOTS_SIZE);
+        const daysRatio = daysDiff / max;
+        const isDotActive = dotRatio <= daysRatio;
+
         return (
           <span
             key={index}
-            className={`${styles.dot} ${
-              (index + 1) * (1 / DOTS_SIZE) <= daysDiff / max ? styles.hot : ""
-            }`}
+            className={`${styles.dot} ${isDotActive ? styles.hot : ""}`}
           />
         );
       })}
