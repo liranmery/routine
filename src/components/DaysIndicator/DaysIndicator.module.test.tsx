838ccbeafba7317ee5127c582501100ad8d55ea9
Liran Mery <liran.mery@gong.io>
2024-01-07 16:29:16 +0200

    complete unit tests for DaysIndicator

--- src/components/DaysIndicator/DaysIndicator.module.test.tsx
+++ src/components/DaysIndicator/DaysIndicator.module.test.tsx
@@ -35,3 +35,78 @@ test("turn on only first dot", () => {
   expect(screen.getAllByTestId("dot")[2]).not.toHaveClass("hot");
   expect(screen.getAllByTestId("dot")[3]).not.toHaveClass("hot");
 });
+
+test("turn on only first two dots", () => {
+  const maxDays = faker.number.int({ min: 10, max: 100 });
+  const date = faker.date.anytime();
+  const halfMaxDays = Math.ceil(maxDays / (DOTS_SIZE / 2));
+
+  render(
+    <DaysIndicator
+      currentDate={addDays(date, halfMaxDays)}
+      date={date}
+      maxDays={maxDays}
+    />
+  );
+
+  expect(screen.getAllByTestId("dot")[0]).toHaveClass("hot");
+  expect(screen.getAllByTestId("dot")[1]).toHaveClass("hot");
+  expect(screen.getAllByTestId("dot")[2]).not.toHaveClass("hot");
+  expect(screen.getAllByTestId("dot")[3]).not.toHaveClass("hot");
+});
+
+test("turn on only first three dots", () => {
+  const maxDays = faker.number.int({ min: 10, max: 100 });
+  const date = faker.date.anytime();
+  const threeQuarterMaxDays = Math.ceil((maxDays / DOTS_SIZE) * 3);
+
+  render(
+    <DaysIndicator
+      currentDate={addDays(date, threeQuarterMaxDays)}
+      date={date}
+      maxDays={maxDays}
+    />
+  );
+
+  expect(screen.getAllByTestId("dot")[0]).toHaveClass("hot");
+  expect(screen.getAllByTestId("dot")[1]).toHaveClass("hot");
+  expect(screen.getAllByTestId("dot")[2]).toHaveClass("hot");
+  expect(screen.getAllByTestId("dot")[3]).not.toHaveClass("hot");
+});
+
+test("turn on only all four dots", () => {
+  const maxDays = faker.number.int({ min: 10, max: 100 });
+  const date = faker.date.anytime();
+
+  render(
+    <DaysIndicator
+      currentDate={addDays(date, maxDays)}
+      date={date}
+      maxDays={maxDays}
+    />
+  );
+
+  expect(screen.getAllByTestId("dot")[0]).toHaveClass("hot");
+  expect(screen.getAllByTestId("dot")[1]).toHaveClass("hot");
+  expect(screen.getAllByTestId("dot")[2]).toHaveClass("hot");
+  expect(screen.getAllByTestId("dot")[3]).toHaveClass("hot");
+});
+
+test("turn off all four dots", () => {
+  const maxDays = faker.number.int({ min: 10, max: 100 });
+  const date = faker.date.anytime();
+  const lessThanQuarterMaxDays = Math.ceil(maxDays / DOTS_SIZE) - 1;
+
+  render(
+    <DaysIndicator
+      currentDate={addDays(date, lessThanQuarterMaxDays)}
+      date={date}
+      maxDays={maxDays}
+    />
+  );
+
+  expect(screen.getAllByTestId("dot")[0]).not.toHaveClass("hot");
+  expect(screen.getAllByTestId("dot")[1]).not.toHaveClass("hot");
+  expect(screen.getAllByTestId("dot")[2]).not.toHaveClass("hot");
+  expect(screen.getAllByTestId("dot")[3]).not.toHaveClass("hot");
+});
